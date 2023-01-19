e064636d50a848afd417dbf1390dfceec864bf77
ryu <114303361+ryuapp@users.noreply.github.com>
2023-01-19 22:33:49 +0900

    Fix undefined title
    If <Title> is undefined or null title,  that error logs are generated.

--- src/components/Title.ts
+++ src/components/Title.ts
@@ -2,22 +2,37 @@ import { ComponentChildren, PreactDOMAttributes } from "preact";
 import { useEffect } from "preact/hooks";
 
 export default function Title({ children }: PreactDOMAttributes) {
-    useEffect(() => {
-        updateTitle(children)
-    },[children]);
-    return null
+  useEffect(() => {
+    checkTitle(children);
+  }, [children]);
+  return null;
 }
 
-const updateTitle = (children: ComponentChildren) => {
-    let newTitle = "";
-    if (Array.isArray(children)) {
-        children.forEach(function (name) {
-            newTitle += String(name)
-        });
-    } else {
-        newTitle = String(children)
-    }
-    if(newTitle !== document.title){
-        document.title = newTitle;
-    }
-}
+const updateTitle = (newTitle: string) => {
+  if (newTitle !== document.title) {
+    document.title = newTitle;
+  }
+};
+const getTitleString = (children: ComponentChildren) => {
+  let newTitle: string = "";
+
+  if (Array.isArray(children)) {
+    children.forEach(function (name) {
+      newTitle += String(name);
+    });
+  } else {
+    newTitle = String(children);
+  }
+
+  return newTitle
+};
+const checkTitle = (children: ComponentChildren) => {
+  if (children === undefined) {
+    throw new Error("New title is undefined");
+  } else if (children === null) {
+    throw new Error("New title is null");
+  }
+
+  const newTitle: string = getTitleString(children);
+  updateTitle(newTitle);
+};
